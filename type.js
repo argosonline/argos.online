f244a826c346fd77884574096f81db3afa336a4f
argosonline <argos.online00@gmail.com>
2024-09-30 19:17:41 +0900

    Add files via upload

--- type.js
+++ type.js
@@ -1,13 +1,87 @@
-const textElement = document.getElementById('text');
-const text = "Argosへようこそ。365日どの端末からでも監視カメラをしようできます";
-let index = 0;
+// ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
 
-function type() {
-    if (index < text.length) {
-        textElement.textContent += text.charAt(index); // 文字を一つずつ追加
-        index++;
-        setTimeout(type, 100); // 次の文字を表示するまでの遅延
+class TextScramble {
+    constructor(el) {
+      this.el = el
+      this.chars = '!<>-_\\/[]{}—=+*^?#________'
+      this.update = this.update.bind(this)
     }
-}
-
-type(); // タイプ関数を呼び出す
+    setText(newText) {
+      const oldText = this.el.innerText
+      const length = Math.max(oldText.length, newText.length)
+      const promise = new Promise((resolve) => this.resolve = resolve)
+      this.queue = []
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || ''
+        const to = newText[i] || ''
+        const start = Math.floor(Math.random() * 40)
+        const end = start + Math.floor(Math.random() * 40)
+        this.queue.push({ from, to, start, end })
+      }
+      cancelAnimationFrame(this.frameRequest)
+      this.frame = 0
+      this.update()
+      return promise
+    }
+    update() {
+      let output = ''
+      let complete = 0
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i]
+        if (this.frame >= end) {
+          complete++
+          output += to
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar()
+            this.queue[i].char = char
+          }
+          output += `<span class="dud">${char}</span>`
+        } else {
+          output += from
+        }
+      }
+      this.el.innerHTML = output
+      if (complete === this.queue.length) {
+        this.resolve()
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update)
+        this.frame++
+      }
+    }
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+  }
+  
+  // ——————————————————————————————————————————————————
+  // Example
+  // ——————————————————————————————————————————————————
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const phrases = [
+      'Neo,',
+      'stream youre video',
+      'you\'re going to realize',
+      'just as I did',
+      'that there\'s a difference',
+      'between knowing the path',
+      'and walking the path'
+    ];
+    
+    const el = document.querySelector('.text');
+    const fx = new TextScramble(el);
+    
+    let counter = 0;
+    const next = () => {
+      fx.setText(phrases[counter]).then(() => {
+        setTimeout(next, 800);
+      });
+      counter = (counter + 1) % phrases.length;
+    };
+    
+    next();
+  });
+  
